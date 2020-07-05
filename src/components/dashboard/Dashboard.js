d163a42cb16a6f029967aa91aadebaf37b2cce57
Ángel Luis Hernández Herrero <angelluishernandezherrero@gmail.com>
2020-07-05 22:51:50 +0200

    TODO: helpers don't load on first load

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import DashboardCard from "./DashboardCard";
+import { useEffect } from "react";
 
 const Dashboard = ({ userUid, helpers, publicHelpers }) => {
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		if (helpers !== undefined || publicHelpers !== undefined) {
+			setLoading(false);
+		}
+
+		//eslint-disable-next-line
+	}, []);
+
 	const helpersJSX = helpers.map((helper, index) => (
 		<DashboardCard helper={helper} key={index} />
 	));
@@ -13,11 +24,17 @@ const Dashboard = ({ userUid, helpers, publicHelpers }) => {
 
 	//Use the dashboard to show the latest helpers
 	return (
-		<div className="container mt-5">
-			<div className="row ">{helpersJSX}</div>
-			<hr className="col-8" />
-			<div className="row">{publicHelpersJSX}</div>
-		</div>
+		<>
+			{loading ? (
+				<h1>Loading...</h1>
+			) : (
+				<div className="container mt-5">
+					<div className="row ">{helpersJSX}</div>
+					<hr className="col-8" />
+					<div className="row">{publicHelpersJSX}</div>
+				</div>
+			)}
+		</>
 	);
 };
 
